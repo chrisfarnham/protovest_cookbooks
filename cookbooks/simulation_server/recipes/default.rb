3046fa4f16aa44d7e70bbb202559c8b21704dfe2
Chris Farnham <chris.farnham@gmail.com>
2016-05-31 21:27:08 +0000

    created directory and added git resource

--- cookbooks/simulation_server/recipes/default.rb
+++ cookbooks/simulation_server/recipes/default.rb
@@ -6,3 +6,19 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+
+
+directory '/home/ec2-user/api' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+
+git '/home/ec2-user/api' do
+  repository 'https://github.com/chrisfarnham/dateliner.git'
+  revision 'master'
+  action :sync
+end
